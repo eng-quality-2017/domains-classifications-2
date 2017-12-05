f2d936864e9eb3177c70f68e651f7b5bc2221a91
shmilovt <shmilovt@post.bgu.ac.il>
2017-12-05 18:15:48 +0200

    new classes

---
+++ src/main/java/domainClassification2/SpoonExample.java
@@ -0,0 +1,10 @@
+package domainClassification2;
+
+public class SpoonExample {
+    public boolean foo(int predicate){
+        if(predicate > 0)
+            return true;
+        return false;
+
+    }
+}
